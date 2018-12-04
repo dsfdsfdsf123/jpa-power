db237fcb3d6504ef4bcd4b62084077a29c8b4fcb
liugang <860721890@qq.com>
2018-12-04 14:41:56 +0800

    关联类，没有加注解

--- src/main/java/com/jpa/jpapower/articlecategory/entity/ArticleCategory.java
+++ src/main/java/com/jpa/jpapower/articlecategory/entity/ArticleCategory.java
@@ -3,15 +3,15 @@ package com.jpa.jpapower.articlecategory.entity;
 import com.jpa.jpapower.article.entity.Article;
 import com.jpa.jpapower.category.entity.Category;
 
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
  * @author liugang
  * @create 2018/12/4 14:28
  **/
+@Entity
+@Table(name = "t_article_category")
 public class ArticleCategory implements Serializable {
     @Id
     @ManyToOne
