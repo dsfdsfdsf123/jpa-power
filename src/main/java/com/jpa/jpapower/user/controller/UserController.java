723c2b7683e0ed1cb35c4e20b7cb50a522e2a286
liugang <860721890@qq.com>
2018-12-04 22:34:12 +0800

    创建一个thymeleaf

---
+++ src/main/java/com/jpa/jpapower/user/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.jpa.jpapower.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author liugang
+ * @create 2018/12/4 22:27
+ **/
+@Controller
+public class UserController {
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        String name = "1234";
+        model.addAttribute("name",name);
+        return "login";
+    }
+}
